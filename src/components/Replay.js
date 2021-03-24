64c044b7a70863c37f9dfcb5136d57d252ea7942
NirushaaaR <nirush_srijaimoon@cmu.ac.th>
2021-03-25 05:18:56 +0700

    prepare replay route

--- src/components/Replay.js
+++ src/components/Replay.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Redirect } from 'react-router';
+import { UserContext } from '../UserProvider';
 
 const Replay = () => {
+
+    const user = useContext(UserContext);
+    const [redirect, setredirect] = useState(null);
+
+    useEffect(() => {
+        if (!user) {
+            setredirect("/");
+        }
+    }, [user]);
+    if (redirect) {
+        <Redirect to={redirect} />;
+    }
+
     return (
         <div>
-            
+            Replay Route
         </div>
     )
 }
